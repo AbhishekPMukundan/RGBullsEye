12f085ff07165ec75fce3cd13f7b2c525f3fce6e
Abhishek <mailabhi390@gmail.com>
2020-08-08 12:21:13 +0100

    SwiftUI Challenge-1

--- RGBullsEye/ContentView.swift
+++ RGBullsEye/ContentView.swift
@@ -9,13 +9,68 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rtarget = Double.random(in: 0..<1)
+    let gtarget = Double.random(in: 0..<1)
+    let btarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    
+    @State var showAlert = false
+    
+    func computeScore() -> Int {
+        let rDiff = rGuess - rtarget
+        let gDiff = gGuess - gtarget
+        let bDiff = bGuess - btarget
+        let diff = sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff)
+        return Int((1.0 - diff) * 100.0 + 0.5)
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            HStack {
+                VStack {
+                    Color(red: rtarget, green: gtarget, blue: btarget)
+                    Text("Match this color")
+                }
+                VStack {
+                    Color(red: rGuess, green: gGuess, blue: bGuess)
+                    Text("R: \(Int(rGuess * 255.0))"
+                        + "  G: \(Int(gGuess * 255.0))"
+                        + "  B: \(Int(bGuess * 255.0))")
+                }
+            }
+            Button(action: {self.showAlert = true }) {
+                Text("Hit Me!")
+            }.alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"), message: Text(String(computeScore())))
+            }.padding()
+            
+            ColorSlider(value: $rGuess, textColor: .red)
+            ColorSlider(value: $gGuess, textColor: .green)
+            ColorSlider(value: $bGuess, textColor: .blue)
+        }
     }
 }
 
+#if DEBUG
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
+            .previewLayout(.fixed(width: 568, height: 320))
+    }
+}
+#endif
+
+struct ColorSlider: View {
+    @Binding var value: Double
+    var textColor: Color
+    
+    var body: some View {
+        HStack {
+            Text("0").foregroundColor(textColor)
+            Slider(value: $value)
+            Text("255").foregroundColor(textColor)
+        }.padding(.horizontal)
     }
 }
